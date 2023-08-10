5403bf197cb799758b98639ddbd98cfa9de751d8
yudichoccolatos <a201511@201511s-MacBook-Air.local>
2023-08-10 19:10:52 +0700

    create category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+
+        return response()->json($categories);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create($request->all());
+        return response()->json([
+            'data' => $category
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        $category->update($request->all());
+
+        return response()->json([
+            'message'   =>'Update Success',
+            'data'      => $category
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response()->json([
+            'message'=>'success'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,5 +12,11 @@ Route::group([
     'prefix'    => 'auth'
 ], function() {
     Route::post('login', [AuthController::class, 'login'])->name('login');
-}
-);
+});
+
+
+Route::group([
+    'Middleware' => 'api'
+], function() {
+    Route::resources(['categories' => CategoryController::class]);
+});
